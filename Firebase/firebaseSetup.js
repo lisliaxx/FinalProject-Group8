2c9ab7f5bc45014c4eb43dae3bd5cd1050226b49
monshanlin <monshanlin@gmail.com>
2024-11-09 19:54:18 -0800

    Add schedule to firebase

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -5,14 +5,14 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDGYTsGTABpu-Z6uV9ncxqJ-8tKG0F3ul0",
-    authDomain: "cs5520-2cead.firebaseapp.com",
-    projectId: "cs5520-2cead",
-    storageBucket: "cs5520-2cead.firebasestorage.app",
-    messagingSenderId: "801980424038",
-    appId: "1:801980424038:web:1af2938570b5b76dc032a7",
-    measurementId: "G-671SFEG3TZ"
-  };
+    apiKey: process.env.EXPO_PUBLIC_apiKey,
+    authDomain: process.env.EXPO_PUBLIC_authDomain,
+    projectId: process.env.EXPO_PUBLIC_projectId,
+    storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+    messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+    appId: process.env.EXPO_PUBLIC_appId,
+    measurementId: process.env.EXPO_PUBLIC_measurementId,
+};
 
 const app = initializeApp(firebaseConfig);
 
@@ -22,4 +22,4 @@ export const database = getFirestore(app);
 
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
-}); 
+})
